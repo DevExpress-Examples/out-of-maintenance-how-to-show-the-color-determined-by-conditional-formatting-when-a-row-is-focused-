3ea10bac3d4c710e014f1819f2dc88fcf20159b9
DevExpressExampleBot <fakeEmail>
2021-09-06 17:05:57 +0300

    Source auto update [skip ci]

--- VB/DXSample/Application.xaml.vb
+++ VB/DXSample/Application.xaml.vb
@@ -3,14 +3,14 @@ Imports System.Linq
 Imports System.Windows
 
 Namespace DXSample
-    ''' <summary>
-    ''' Interaction logic for App.xaml
-    ''' </summary>
-    Partial Public Class App
-        Inherits Application
+	''' <summary>
+	''' Interaction logic for App.xaml
+	''' </summary>
+	Partial Public Class App
+		Inherits Application
 
-        Private Sub OnAppStartup_UpdateThemeName(ByVal sender As Object, ByVal e As StartupEventArgs)
-            DevExpress.Xpf.Core.ApplicationThemeHelper.UpdateApplicationThemeName()
-        End Sub
-    End Class
+		Private Sub OnAppStartup_UpdateThemeName(ByVal sender As Object, ByVal e As StartupEventArgs)
+			DevExpress.Xpf.Core.ApplicationThemeHelper.UpdateApplicationThemeName()
+		End Sub
+	End Class
 End Namespace
--- VB/DXSample/MainWindow.xaml.vb
+++ VB/DXSample/MainWindow.xaml.vb
@@ -2,17 +2,17 @@
 Imports DXSample.Common
 
 Namespace DXSample
-    Partial Public Class MainWindow
-        Inherits DXWindow
+	Partial Public Class MainWindow
+		Inherits DXWindow
 
-        Public Sub New()
-            InitializeComponent()
-            DataContext = DataHelper.GetRows()
-        End Sub
+		Public Sub New()
+			InitializeComponent()
+			DataContext = DataHelper.GetRows()
+		End Sub
 
-        Private Sub TableView_CustomRowAppearance(ByVal sender As Object, ByVal e As DevExpress.Xpf.Grid.CustomRowAppearanceEventArgs)
-            e.Result = e.ConditionalValue
-            e.Handled = True
-        End Sub
-    End Class
+		Private Sub TableView_CustomRowAppearance(ByVal sender As Object, ByVal e As DevExpress.Xpf.Grid.CustomRowAppearanceEventArgs)
+			e.Result = e.ConditionalValue
+			e.Handled = True
+		End Sub
+	End Class
 End Namespace
