50e0193e8eff8920b2d512744d2ab245076950f4
DevExpressExampleBot <fakeEmail>
2023-06-22 12:17:30 +0400

    Source auto update [skip ci]

--- VB/DXSample/MainWindow.xaml.vb
+++ VB/DXSample/MainWindow.xaml.vb
@@ -1,18 +1,19 @@
-﻿Imports DevExpress.Xpf.Core
+Imports DevExpress.Xpf.Core
 Imports DXSample.Common
 
 Namespace DXSample
-	Partial Public Class MainWindow
-		Inherits DXWindow
 
-		Public Sub New()
-			InitializeComponent()
-			DataContext = DataHelper.GetRows()
-		End Sub
+    Public Partial Class MainWindow
+        Inherits DXWindow
 
-		Private Sub TableView_CustomRowAppearance(ByVal sender As Object, ByVal e As DevExpress.Xpf.Grid.CustomRowAppearanceEventArgs)
-			e.Result = e.ConditionalValue
-			e.Handled = True
-		End Sub
-	End Class
+        Public Sub New()
+            Me.InitializeComponent()
+            DataContext = GetRows()
+        End Sub
+
+        Private Sub TableView_CustomRowAppearance(ByVal sender As Object, ByVal e As DevExpress.Xpf.Grid.CustomRowAppearanceEventArgs)
+            e.Result = e.ConditionalValue
+            e.Handled = True
+        End Sub
+    End Class
 End Namespace
